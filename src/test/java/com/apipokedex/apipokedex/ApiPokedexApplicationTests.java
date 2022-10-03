5ea4a683a844283aa8136deb578f11a12eab9def
MoisesDelfino <moisescamposdelfino@gmail.com>
2022-10-03 20:42:18 -0300

    Inicialização novamente, agora vai

---
+++ src/test/java/com/apipokedex/apipokedex/ApiPokedexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apipokedex.apipokedex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPokedexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
